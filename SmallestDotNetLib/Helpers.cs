759b22537095fb3b01019b1105691451f99e2acb
Jason Deering <deeringj2@gmail.com>
2012-09-22 09:00:17 -0400

    Cleaning up mistakes

--- SmallestDotNetLib/Helpers.cs
+++ SmallestDotNetLib/Helpers.cs
@@ -76,7 +76,7 @@ public class Helpers
         return netInfoString;
     }
 
-    private static bool CheckDotNet4Installed(ref string userMessage)
+    private static bool CheckDotNet4Installed(string UserAgent, ref string userMessage)
     {
         if (UserAgent.Contains(".NET4.0E"))
         {
@@ -135,15 +135,14 @@ public class Helpers
         switch (build)
         {
             case 21022: //RTM
-                netInfoString += String.Format("Looks like you {2} have <strong>.NET version 3.5</strong>. The latest version is 3.5 SP1. You can upgrade quickly with this small download for {0}. Also, you should make sure your system is setup to get updates from {1} automatically. This will make sure your system is up to date with the lastest stuff, including the latest .NET Framework.", Constants.dotnet35online, Constants.windowsupdate, hasDotNet4 ? "also" : "");
-                break;
+                return String.Format("Looks like you {2} have <strong>.NET version 3.5</strong>. The latest version is 3.5 SP1. You can upgrade quickly with this small download for {0}. Also, you should make sure your system is setup to get updates from {1} automatically. This will make sure your system is up to date with the lastest stuff, including the latest .NET Framework.", Constants.dotnet35online, Constants.windowsupdate, hasDotNet4 ? "also" : "");
             case 30729: //SP1
-                netInfoString += String.Format("Looks like you {1} have <strong>.NET version 3.5 SP1</strong>. That's the VERY latest .NET Framework. <strong>You don't need to do anything right now.</strong> However, you might make sure your system is setup to get updates from {0} automatically. This will make sure your system is up to date with the lastest stuff, including the latest .NET Framework.", Constants.windowsupdate, hasDotNet4 ? "also" : "");
-                break;
+                return String.Format("Looks like you {1} have <strong>.NET version 3.5 SP1</strong>. That's the VERY latest .NET Framework. <strong>You don't need to do anything right now.</strong> However, you might make sure your system is setup to get updates from {0} automatically. This will make sure your system is up to date with the lastest stuff, including the latest .NET Framework.", Constants.windowsupdate, hasDotNet4 ? "also" : "");
             default:
-                netInfoString += String.Format("Looks like you <i>might</i> {2} have a <em>beta</em> version of <strong>.NET version 3.5 SP1</strong>. Perhaps you're a programmer or you know one? You should probably uninstall that version and run the small setup program for {0}. Also, you might make sure your system is setup to get updates from {1} automatically. This will make sure your system is up to date with the lastest stuff, including the latest .NET Framework.", Constants.dotnet35online, Constants.windowsupdate, hasDotNet4 ? "also" : "");
-                break;
+                return String.Format("Looks like you <i>might</i> {2} have a <em>beta</em> version of <strong>.NET version 3.5 SP1</strong>. Perhaps you're a programmer or you know one? You should probably uninstall that version and run the small setup program for {0}. Also, you might make sure your system is setup to get updates from {1} automatically. This will make sure your system is up to date with the lastest stuff, including the latest .NET Framework.", Constants.dotnet35online, Constants.windowsupdate, hasDotNet4 ? "also" : "");
         }
+
+        return "";
     }
 
     private static void CheckDotNet3_5UnSupportedOs(string UserAgent, string agent, string friendlyName, ref string userMessage)
