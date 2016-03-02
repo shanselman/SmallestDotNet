shanselman/SmallestDotNet

commit df18da8b20fe4b08420fa5f38fda36684d6ed362

Better handling of unknown release keys in registry
Files missed in last commit

SmallestDotNetLib/Helpers.cs
@@ -7,7 +7,12 @@ using System.Collections.Generic;
 /// </summary>
 public class Helpers
 {
-    public static UpdateInformationResponse GetUpdateInformation(string UserAgent, Version version)
+    public static UpdateInformationResponse GetUpdateInformation(string userAgent)
+    {
+        return GetUpdateInformation(userAgent, null, 0);
+    }
+    
+    public static UpdateInformationResponse GetUpdateInformation(string UserAgent, string realVersion, int releaseKey)
     {
         bool net4 = false;
         string netInfoString = "";
@@ -30,12 +35,16 @@ public class Helpers
             response.Text = "It looks like you're running a Unix machine. There's no .NET Framework download from Microsoft for Unix, but you might check out <a href=\"http://www.go-mono.com/mono-downloads/download.html\">Mono</a>, which is an Open Source platform that can run .NET code on Unix.";
             return response;
         }
-        
+
         response.CanRunCheckApp = true;
         response.VersionCanBeDetermined = true;
 
         net4 = GetWindows8Message(UserAgent, ref netInfoString) || Get40Message(UserAgent, ref netInfoString);
-        if (Helpers.Has35(UserAgent) || Helpers.Has35SP1C(UserAgent) || Helpers.Has35SP1E(UserAgent))
+        if (!string.IsNullOrEmpty(realVersion))
+        {
+            netInfoString = GetRealVersionMessage(realVersion, releaseKey);
+        }
+        else if (Helpers.Has35(UserAgent) || Helpers.Has35SP1C(UserAgent) || Helpers.Has35SP1E(UserAgent))
         {
             netInfoString += DotNet3_5Message((Helpers.Has35SP1C(UserAgent) || Helpers.Has35SP1E(UserAgent)), net4);
         }
@@ -81,6 +90,19 @@ public class Helpers
 
         response.Text = netInfoString;
         return response;
+    }
+
+    /// <summary>
+    /// Gets a message based on the "real" version as determined by the .Net Checker application.
+    /// </summary>
+    /// <param name="realVersion">The real version.</param>
+    /// <param name="releaseKey">The release key (will be zero in cases where the release key was exactly matched to a .Net Version).</param>
+    /// <returns>A message like "The .Net Checker application determined that you've got X.Y on your machine."</returns>
+    private static string GetRealVersionMessage(string realVersion, int releaseKey)
+    {
+        return releaseKey > 0
+            ? string.Format(Constants.CheckerFound + Constants.CheckerFoundNotExact, realVersion, releaseKey)
+            : string.Format(Constants.CheckerFound, realVersion);
     }
 
     private static bool GetWindows8Message(string UserAgent, ref string userMessage)
@@ -302,5 +324,6 @@ public class Helpers
     {
         return UserAgent.Contains(Constants.Version10Full);
     }
+
 
 }

CheckForDotNet45/Program.cs
@@ -64,6 +64,7 @@ namespace CheckForDotNet45
                     if (releaseKey > 381029)
                     {
                         exact = false;
+                        return "4.6 or greater";
                     }
                 }
             }

SmallestDotNet/Default.aspx.cs
@@ -7,21 +7,21 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string userAgentText;
+        string realVersion = null;
+        int releaseKey = 0;
         bool runFromChecker = false;
-        userAgent.Text = HttpUtility.HtmlEncode(Request.UserAgent);
+        string userAgentText = this.Request.UserAgent ?? string.Empty;
+        userAgent.Text = HttpUtility.HtmlEncode(userAgentText);
 
         if (Request.QueryString["realversion"] != null)
         {
-            userAgentText = this.Request.QueryString["realversion"];
+            realVersion = this.Request.QueryString["realversion"];
+            int.TryParse(this.Request.QueryString["releaseKey"], out releaseKey);
             runFromChecker = true;
         }
-        else
-        {
-            userAgentText = this.Request.UserAgent;
-        }
 
-        UpdateInformationResponse response = Helpers.GetUpdateInformation(userAgentText, this.Request.Browser.ClrVersion);
+
+        UpdateInformationResponse response = Helpers.GetUpdateInformation(userAgentText, realVersion, releaseKey);
 
         userResult.Text = response.Text;
         developerOnline.Text = String.Format(@"If your users have internet connectivity, the .NET Framework is only between {1} and {2} megs. Why such a wide range? Well, it depends on if they already have some version of .NET.

SmallestTest/VersionCheckTest.cs
@@ -1,5 +1,7 @@
 ﻿namespace SmallestTest
 {
+    using System.Globalization;
+
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System;
     using System.Collections.Generic;
@@ -42,6 +44,40 @@
             var userAgent = "Windows NT 6.2";
 
             Assert.IsTrue(Helpers.HasWindows8(userAgent), "Windows 8");
+            userAgent = "Windows NT 6.3";
+
+            Assert.IsTrue(Helpers.HasWindows8(userAgent), "Windows 8.1");
+        }
+
+        [TestMethod]
+        public void CheckRealVersion()
+        {
+            // Arrange
+            const string UserAgent = ".NET Version 1.0";
+            const string RealVersion = "4.5.1";
+            const string CheckerApplicationText = "The .Net Checker application determined that you have";
+            const string UnableToDetermineText = "The application was not able to determine the exact version you have.";
+            var releaseKey = 0;
+
+            // Act
+            string message = Helpers.GetUpdateInformation(UserAgent, RealVersion, releaseKey).Text;
+
+            // Assert
+            StringAssert.StartsWith(message, CheckerApplicationText);
+            StringAssert.Contains(message, RealVersion);
+            Assert.IsFalse(message.Contains(UnableToDetermineText), "The '...not able to determine the exact version...' message should NOT appear if releaseKey=0");
+   
+            // Arrange
+            releaseKey = 9999;
+
+            // Act
+            message = Helpers.GetUpdateInformation(UserAgent, RealVersion, releaseKey).Text;
+
+            // Assert
+            StringAssert.StartsWith(message, CheckerApplicationText);
+            StringAssert.Contains(message, RealVersion);
+            StringAssert.Contains(message, UnableToDetermineText, "The '...not able to determine the exact version...' message SHOULD appear if releaseKey>0");
+            StringAssert.Contains(message, releaseKey.ToString(CultureInfo.InvariantCulture));
         }
     }
 }

commit 9af1081390350413efd4673e1ead95f530e6d37a

javascript.ashx CS1501 compilation error fix
Fixes issue #70
Possibly was broken since df18da8b20fe4b08420fa5f38fda36684d6ed362

SmallestDotNet/javascript.ashx
@@ -11,7 +11,7 @@ public class SmallestDotNet : IHttpHandler
         WriteToResponse("<span class=\"smallerdotnet\">");
         context.Response.ContentType = "text/javascript"; 
 
-        WriteToResponse(Helpers.GetUpdateInformation(context.Request.UserAgent, context.Request.Browser.ClrVersion).Text.Replace("'", @"\'").Replace(Environment.NewLine, "<br />"));
+        WriteToResponse(Helpers.GetUpdateInformation(context.Request.UserAgent, context.Request.Browser.ClrVersion.ToString()).Text.Replace("'", @"\'").Replace(Environment.NewLine, "<br />"));
         WriteToResponse("</span>");
     }
 

SmallestDotNetLib/Helpers.cs
@@ -13,6 +13,11 @@ public class Helpers
         return GetUpdateInformation(userAgent, null, 0);
     }
 
+    public static UpdateInformationResponse GetUpdateInformation(string UserAgent, string realVersion)
+    {
+        return GetUpdateInformation(UserAgent, realVersion, 0);
+    }
+
     public static UpdateInformationResponse GetUpdateInformation(string UserAgent, string realVersion, int releaseKey)
     {
         bool net4 = false;